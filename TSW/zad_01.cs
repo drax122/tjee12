aced2b1bbe7865656ac624cc72b797c7812f9f41
draxeer <xeonxx@o2.pl>
2017-03-03 19:03:24 +0100

    zad 1-5

---
+++ TSW/zad_01.cs
@@ -0,0 +1,24 @@
+.borderA {
+  border: solid 1px red;
+}
+.borderB {
+  border: solid 1px blue;
+}
+.A {
+  border: solid 1px red;
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  right: auto;
+  left: auto;
+  vertical-align: middle;
+}
+.B {
+  border: solid 1px blue;
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  right: auto;
+  left: auto;
+  vertical-align: middle;
+}
